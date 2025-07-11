b3ad5c52315930acf546899715ddf156cb4e4703
Dilip290 <dilipbojanapu290@gmail.com>
2025-07-11 13:16:29 +0530

    Update urlRoutes.ts

--- src/routes/urlRoutes.ts
+++ src/routes/urlRoutes.ts
@@ -1,1 +1,10 @@
+import express from 'express';
+import { UrlController } from '../controllers/UrlController';
 
+const router = express.Router();
+
+router.post('/shorturls', UrlController.createShortUrl);
+router.get('/shorturls/:shortcode', UrlController.getUrlStats);
+router.get('/:shortcode', UrlController.redirectUrl);
+
+export default router;
