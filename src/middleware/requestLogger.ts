0fc27782540f043f23e15ae6ddd361e054784ff4
Dilip290 <dilipbojanapu290@gmail.com>
2025-07-11 13:14:54 +0530

    Update requestLogger.ts

--- src/middleware/requestLogger.ts
+++ src/middleware/requestLogger.ts
@@ -1,1 +1,14 @@
+import { Request, Response, NextFunction } from 'express';
+import Log from '../../../Logging Middleware/logger';
 
+export const requestLogger = (req: Request, res: Response, next: NextFunction): void => {
+  const startTime = Date.now();
+
+  res.on('finish', () => {
+    const duration = Date.now() - startTime;
+    const logMessage = `${req.method} ${req.path} - ${res.statusCode} - ${duration}ms`;
+    Log('RequestLogger', 'INFO', 'url-shortener', logMessage);
+  });
+
+  next();
+};
